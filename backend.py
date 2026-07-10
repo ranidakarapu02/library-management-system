6e44d9da507a3971c88205421807d1a4ec86fda5
dakarapu rani <ranidakarapu7@gmail.com>
2026-07-10 13:10:32 +0530

    Add backend.py

---
+++ backend.py
@@ -0,0 +1,38 @@
+# from tkinter import *
+
+# root = Tk()
+# root.title("Library Management System")
+# root.geometry("500x400")
+
+# Label(root, text="Library Management System",
+#       font=("Arial", 16, "bold")).pack(pady=10)
+
+# Label(root, text="Book Name").pack()
+
+# book_entry = Entry(root, width=30)
+# book_entry.pack(pady=5)
+
+# root.mainloop()
+from tkinter import *
+
+root = Tk()
+root.title("Library Management System")
+root.geometry("500x400")
+
+Label(root, text="Library Management System",
+      font=("Arial", 16, "bold")).pack(pady=10)
+
+Label(root, text="Book Name").pack()
+
+book_entry = Entry(root, width=30)
+book_entry.pack(pady=5)
+
+books = []
+
+def add_book():
+    book = book_entry.get()
+    books.append(book)
+    print("Book Added:", book)
+    book_entry.delete(0, END)
+
+root.mainloop()
