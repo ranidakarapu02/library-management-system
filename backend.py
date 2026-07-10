7655800508104755b1e03f620291f39f4c8d4e99
dakarapu rani <ranidakarapu7@gmail.com>
2026-07-10 16:12:12 +0530

    Remove mainn.py

--- backend.py
+++ backend.py
@@ -1,18 +1,3 @@
-# from tkinter import *
-
-# root = Tk()
-# root.title("Library Management System")
-# root.geometry("500x400")
-
-# Label(root, text="Library Management System",
-#       font=("Arial", 16, "bold")).pack(pady=10)
-
-# Label(root, text="Book Name").pack()
-
-# book_entry = Entry(root, width=30)
-# book_entry.pack(pady=5)
-
-# root.mainloop()
 from tkinter import *
 
 root = Tk()
@@ -34,5 +19,7 @@ def add_book():
     books.append(book)
     print("Book Added:", book)
     book_entry.delete(0, END)
+
+Button(root, text="Add Book", command=add_book).pack(pady=10)
 
 root.mainloop()
